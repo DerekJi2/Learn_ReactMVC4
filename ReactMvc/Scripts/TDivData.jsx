b93ea0cb47c21042076cfd6697a1091bb48e524d
Derek Ji <derek.ji@virtualagent.com.au>
2017-11-23 21:41:44 +1030

    MyDemo: TDivData & Render 2 components

---
+++ ReactMvc/Scripts/TDivData.jsx
@@ -0,0 +1,34 @@
+﻿var TDivData = React.createClass({
+    getInitialState: function() {
+        return {
+            data: "",
+            hiddenClass: (this.props.hidden == "true") ? "hidden" : ""
+        };
+    },
+
+    componentDidMount: function() {
+        $.get(this.props.source, function(response){
+            if (this.isMounted()) {
+                this.setState({
+                    data: response
+                });
+            }
+        }.bind(this));
+    },
+
+    render: function() {
+        return (
+            <div id="dv-account-json" className={this.state.hiddenClass}>{this.state.data}</div>
+        );
+    }
+});
+
+ReactDOM.render(
+  <TDivData source="/Content/json/accountJSON.html" hidden="false"/>,
+  document.getElementById('divdata1')
+);
+
+ReactDOM.render(
+  <TDivData source="/Home/Index" hidden="false"/>,
+  document.getElementById('divdata2')
+);
