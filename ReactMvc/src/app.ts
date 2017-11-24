ba8c1ffc73157468b4a992471841933ffa32fb94
Derek Ji <derek.ji@virtualagent.com.au>
2017-11-25 09:39:30 +1030

    Typescript: exercise 01 with jQuery

--- ReactMvc/src/app.ts
+++ ReactMvc/src/app.ts
@@ -1,5 +1,16 @@
 ﻿function sayHello() {
     const compiler = (document.getElementById("compiler") as HTMLInputElement).value;
     const framework = (document.getElementById("framework") as HTMLInputElement).value;
-    return `Hello Derek! This is from ${compiler} and ${framework}!`;
+    return `Hello Derek Ji! This is from ${compiler} and ${framework}!`;
 }
+
+function greeter(person: string) {
+    return "Hello, " + person;
+}
+
+let user = "Jane User";
+
+let greeter_string = greeter(user);
+
+
+//(document.getElementById("greeter") as HTMLInputElement).value(greeter(user));
